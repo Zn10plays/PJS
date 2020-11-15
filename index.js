aac24d7e53f99d2af88b44dd37d7b85794e6ede0
A68AGaming <68760804+A68AGaming@users.noreply.github.com>
2020-11-15 17:45:25 +0100

    made some shit better (i hope it dont break)

--- index.js
+++ index.js
@@ -13,42 +13,32 @@ client.on('ready', () => {
 
 
 
-let test = new Discord.MessageEmbed(config.example.test)
-let pathfinder = new Discord.MessageEmbed(config.example.pathfinder)
-let sleep = new Discord.MessageEmbed(config.example.sleep)
-let pvp = new Discord.MessageEmbed(config.example.pvp)
-let chatEvent = new Discord.MessageEmbed(config.example.chat)
+
 let font = new Discord.MessageEmbed(config.code)
 
 
 
 function listen(message) {
   if (message.author.id!='336575271144783872') return
-  const args = message.content.split(' ');
+  let args = message.content.split(' ');
   if (message.content.startsWith('$pjs')) {
+    args.shift()
+    let cmd = args[0]
+    args.shift()
     console.log(`${message.author.tag} said: ${message.content}`)
-    if (args[1]=='example') {
-      switch(args[2]){
-        case 'pathfinder':
-          message.channel.send(pathfinder);
-          break;
-        case 'sleep':
-          message.channel.send(sleep);
-          break;
-        case 'pvp':
-          message.channel.send(pvp);
-          break;
-        case 'chat':
-          message.channel.send(chatEvent);
-      }
+    if (cmd=='example') {
+      let example = args[0]
+      if (config.example[example]) msg.channel.send(config.example[example]
+      else {message.channel.send("The requested example was not found.")}
+    }
         
-    }else if (args[1]=='codeFont') {
+    }else if (cmd=='codeFont') {
       message.channel.send(font)
-    }else if (args[1]=='purge') {
-      if (!args[2]) return message.channel.send('wrong args')
-      message.channel.bulkDelete(args[2])
-      message.channel.send('Deleated '+args[2]+' messages')
-    }else if (args[1]=='test'){
+    }else if (cmd=='purge') {
+      if (!args[0]) return message.channel.send('wrong args')
+      message.channel.bulkDelete(args[0])
+      message.channel.send('Deleated '+args[0]+' messages')
+    }else if (cmd=='test'){
       message.channel.send(test)
       console.log('message sent')
     }
