0743fd310bc4c5586f0d634e0d26ad2b2c8ea033
dada513 <kontakt@dada513.eu>
2020-11-15 21:29:58 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ src/commands/test.js
@@ -0,0 +1,23 @@
+import { Command } from "discord.js-commando";
+
+export default class MeowCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "test",
+      group: "prismarinejs",
+      memberName: "test",
+      description: "Hello, world.",
+      args: [
+        {
+          type: "string",
+          prompt: "Who to greet?",
+          key: "greeter",
+        },
+      ],
+    });
+  }
+
+  run(message, { greeter }) {
+    return message.say(`Hello, ${greeter}`);
+  }
+}
